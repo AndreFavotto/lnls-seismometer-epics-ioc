a9da790f2596eb6315388008e82f66e5378c7abf
Ándre de Oliveira Águila Favoto <andre.favoto@cnpem.br>
2022-07-22 14:59:43 -0300

    Removed start.txt dependance

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
 """
-    @authors: Leonardo Rossi Leão / Rodrigo de Oliveira Neto / André de Oliveira Águila Favoto
-    @create: november, 30, 2020
-    @modified: july, 2022
+    @author: André de Oliveira Águila Favoto
+    @create: july, 2022
     @title: main
 """
-
-# Libraries
-import time
 from rawFileMonitor import RawFileMonitor
-from iocSeismometer import ioc 
 
-# Start the software
-file = open("start.txt", "r")
-start = bool(file.read())
-file.close()
-
-if start == True:
-    # Instantiate classes
-    fileMonitor = RawFileMonitor()
-    iocServer = ioc()
-    fileMonitor.start()
-    while start == True:
-        time.sleep(60)
-        file = open("start.txt", "r")
-        start = bool(int(file.read()))
-        file.close()
-
-fileMonitor.stop()
-iocServer.stop()
+fileMonitor = RawFileMonitor()
+fileMonitor.start()
