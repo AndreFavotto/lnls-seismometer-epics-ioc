69ccecceaa04b7362e505215fc39561a124c4222
Ándre de Oliveira Águila Favoto <andre.favoto@cnpem.br>
2022-08-31 08:00:41 -0300

    Added file cleaning and exception handler

--- rawFileMonitor.py
+++ rawFileMonitor.py
@@ -5,8 +5,7 @@
     @title: File monitor
 """
 
-import os, threading
-from datetime import datetime
+import os, threading, shutil, datetime
 from datFileMonitor import DatFileMonitor
 
 class RawFileMonitor(threading.Thread):
@@ -18,29 +17,44 @@ class RawFileMonitor(threading.Thread):
         self.path_cvt = self.path_in + '/../pas2asc'
         self.datFileMonitor = DatFileMonitor()
         
-    def searchFiles(self):
-        today = datetime.now()
+    def scanFiles(self):
+        today = datetime.datetime.now()
         year = today.year
-        dayOfYear = (today - datetime(year, 1, 1)).days + 1
-        path = f'{self.path_in}/{year}{dayOfYear}/B67D/1/'
-        arquivos = set(os.listdir(path))
-        return (path, arquivos)
+        dayOfYear = (today - datetime.datetime(year, 1, 1)).days + 1
+        filesPath = f'{self.path_in}/{year}{dayOfYear}/B67D/1/'
+        arquivos = set(os.listdir(filesPath))
+        return (filesPath, arquivos)
     
-    def conversao(self, path, newFiles):
+    def cleanFiles(self):
+        try: #delete last month's local files if not deleted yet
+            lastMonth = datetime.datetime.now() - datetime.timedelta(days=30)
+            lmYear = lastMonth.year
+            lmDayOfYear = (lastMonth- datetime.datetime(lmYear, 1, 1)).days + 1
+            oldPath = f'{self.path_in}/{lmYear}{lmDayOfYear}'
+            shutil.rmtree(oldPath)
+        except FileNotFoundError:
+            pass
+
+    def convert(self, filesPath, newFiles):
         for file in newFiles:
-            path_in = path + file
+            path_in = filesPath + file
             if '_00000000' not in path_in: # xxxxxxx_00000000: File is still processing
                 os.system(self.path_cvt + ' -Ln ' + path_in)
                 os.remove(path_in)
-                os.system('clear')
                 self.datFileMonitor.run()
         
     def run(self):
-        path, content = self.searchFiles()
-        while not self.kill.is_set():
-            path, newContent = self.searchFiles()
-            newFiles = newContent.difference(content)
-            # Converts data if new file found
-            if newFiles:
-                self.conversao(path, newFiles)
-            content = newContent
+        try:
+            filesPath, content = self.scanFiles()
+            while not self.kill.is_set():
+                self.cleanFiles()
+                filesPath, newContent = self.scanFiles()
+                newFiles = newContent.difference(content)
+                # Converts data if new file found
+                if newFiles:
+                    self.convert(filesPath,newFiles)
+                content = newContent
+        except Exception as e:
+            print(f'Error in raw file monitoring: {e.args[0]}')
+            #logger = logging.handlers.RotatingFileHandler()
+            os._exit(1)
