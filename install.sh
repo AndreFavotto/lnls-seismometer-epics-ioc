6aded1a04520635576a6118483d92f4ef742f5a2
Ándre de Oliveira Águila Favoto <andre.favoto@cnpem.br>
2022-09-09 12:10:25 -0300

    Automated systemd service creation

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@
 SERVICE_NAME=docker-ioc
 
 export APP_DIR="$PWD"
-envsubst < ./template.service.tmpl > ${SERVICE_NAME}.service
-cp ./${SERVICE_NAME}.service /etc/systemd/system
+envsubst < ./template.service.tmpl > /etc/systemd/system/${SERVICE_NAME}.service
 
 systemctl enable ${SERVICE_NAME}.service
 systemctl start ${SERVICE_NAME}.service
