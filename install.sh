a8eda17916dd4187b6191b07c50c723d9f8e6904
Ándre de Oliveira Águila Favoto <andre.favoto@cnpem.br>
2022-09-09 13:32:24 -0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,12 @@
 SERVICE_NAME=docker-ioc
 
 export APP_DIR="$PWD"
-envsubst < ./template.service.tmpl > /etc/systemd/system/${SERVICE_NAME}.service
 
-systemctl enable ${SERVICE_NAME}.service
-systemctl start ${SERVICE_NAME}.service
+if envsubst < ./template.service.tmpl > /etc/systemd/system/${SERVICE_NAME}.service
+then
+    systemctl enable ${SERVICE_NAME}.service
+    systemctl start ${SERVICE_NAME}.service
+    echo "Created and started ${SERVICE_NAME}.service"
+else
+    echo "Failed to create ${SERVICE_NAME}.service"
+fi
